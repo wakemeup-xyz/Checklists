314a068ec37c73e0cd2979bad5ff9c7b0da5c558
Zhang Xiangyu <wakemeup@zhangdemacbook-pro.local>
2016-01-06 21:49:48 +0800

    Initial Commit

---
+++ Checklists/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Checklists
+//
+//  Created by Zhang Xiangyu on 16/1/6.
+//  Copyright © 2016年 Zhang Xiangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Checklists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Checklists
+//
+//  Created by Zhang Xiangyu on 16/1/6.
+//  Copyright © 2016年 Zhang Xiangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
